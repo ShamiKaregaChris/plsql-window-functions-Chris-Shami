9b56106db240549f312bf230d02ae9daf574335c
ShamiKaregaChris <shamichris12@gmail.com>
2025-09-29 11:25:59 +0200

    Update 01_schema_creation.sql

--- 01_schema_creation.sql
+++ 01_schema_creation.sql
@@ -1,14 +1,14 @@
 -- Create database tables for Café Kivu business
 CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
-    name VARCHAR(25),
-    region VARCHAR(30)
+    name VARCHAR(100),
+    region VARCHAR(50)
 );
 
 CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
-    name VARCHAR(25),
-    category VARCHAR(30)
+    name VARCHAR(100),
+    category VARCHAR(50)
 );
 
 CREATE TABLE transactions (
